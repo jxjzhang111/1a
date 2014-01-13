80aa632363a3df927dffc0d2cf3fa0e2243a325f
Jennifer Zhang <jxzhang@post.harvard.edu>
2014-01-13 10:27:33 -0800

    placeholders for lion

--- execute-command.c
+++ execute-command.c
@@ -20,5 +20,6 @@ execute_command (command_t c, int time_travel)
   /* FIXME: Replace this with your implementation.  You may need to
      add auxiliary functions and otherwise modify the source code.
      You can also use external functions defined in the GNU C Library.  */
+	c; time_travel;
   error (1, 0, "command execution not yet implemented");
 }
