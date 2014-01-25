95fe5877ac1715c3613a6e20e9088c9bcc3583da
Jennifer Zhang <jxzhang@post.harvard.edu>
2014-01-25 12:56:04 -0800

    Added test cases Fixed IO output typo

--- shell_test.sh
+++ shell_test.sh
@@ -1,1 +1,8 @@
-foo || (wc && sort && wc -l) < input.txt > output.txt
+#foo || (wc && sort && wc -l) < input.txt > output.txt
+ls -l > test.txt
+ps | grep bash | sort
+(echo b; echo c; echo a) | sort
+foo || echo failure to foo
+echo fooing && foo
+foo && echo shouldnotrun
+sort < test.txt
